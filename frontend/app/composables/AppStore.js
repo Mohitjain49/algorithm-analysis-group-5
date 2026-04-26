b9324918b5c4e997320929846e5f3c8ddf9b3122
Mohitjain49 <mohitkjain49@gmail.com>
2026-04-26 19:19:32 -0400

    Change Version Number + Add tiles as array.

--- frontend/app/composables/AppStore.js
+++ frontend/app/composables/AppStore.js
@@ -1,5 +1,6 @@
 export const useAppStore = defineStore('app-store', () => {
     const gameStatus = ref(0);
+    const tiles = ref([0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
     /**
      * This function sets what status the game is on.
@@ -9,5 +10,15 @@ export const useAppStore = defineStore('app-store', () => {
         gameStatus.value = status;
     }
 
-    return { gameStatus, setGameStatus }
+    /**
+     * This function sets a specific tile to a specific state.
+     * @param {0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8} index The tile to set.
+     * @param {0 | 1 | 2 | 3 | 4} num The status to set as a number.
+     */
+    function setTile(index, num = 0) {
+        if((index < 0 || index > 8) || (num < 0 || num > 4)) { return; }
+        tiles.value[index] = num;
+    }
+
+    return { gameStatus, tiles, setGameStatus, setTile }
 });
